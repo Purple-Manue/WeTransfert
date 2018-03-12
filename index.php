200341a64ba1df48d7dad35e5bcb9cb083b568ea
Chris Gold <chris.ratsimba87@gmail.com>
2018-03-12 10:21:54 +0100

    chris

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-    <html>
-        <head>
-            <?php include 'includes/header.html' ?>
-            <title></title>
+<?php
+include 'Model/sqlConnect.php' ;
+?>
 
-        </head>
-        <body>
-            
-            <?php include 'includes/base_js.html' ?>
-        </body>
-    </html>
+<form action="post" name="connect">
+
+    <fieldset id="registered">
+        <input type="text" name="nom">
+        <input type="text" name="pass">
+        <input type="submit" value="Se connecter">
+        <input type="submit" value="S'inscrire">
+    </fieldset>
+
+</form>
+
+
+<form action="post" name="upload">
+
+    <fieldset id="file">
+        <input type="file" name="file">
+        <input type="submit" value="Envoyer">
+    </fieldset>
+
+</form>
