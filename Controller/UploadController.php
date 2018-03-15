399bb3d9e24e0262b3b72a65f65612a528f9edcf
Silv3rgr3g <silver.gr3g@gmail.com>
2018-03-15 14:51:08 +0100

    merge de la mort

--- Controller/UploadController.php
+++ Controller/UploadController.php
@@ -8,11 +8,9 @@ function save($bdd, $target_file, $user)
     if (isset($_SESSION['id'])) {
         $user = $_SESSION['id'];
         header("location: ../index.php?user=$user");
-        return $user;
     } else {
         $user = 3;
         header("location: ../index.php?user=$user");
-        return $user;
     }
     $name = $_POST['fileName'];
     $date = date('Y-m-d H:i:s');
@@ -30,7 +28,6 @@ function save($bdd, $target_file, $user)
     }
 }
 
-
   if (isset($_POST) AND !empty($_POST)){
     $bdd = connect();
     //////Verification de l'ID USER
@@ -41,11 +38,8 @@ function save($bdd, $target_file, $user)
     }
     $failed = "";
     $size_file = $_FILES['file']['size'];
-    $target_dir = "../docs/";
-    $target_file = $target_dir . basename($_FILES["file"]["name"]);
     //Verification taille fichier
     if(isset($_FILES) AND ($_FILES['file']['error'] == 1)){
-      echo "USR=" . "$user" . "<br>";
       echo "Votre systeme ne vous permet pas d'envoyer un fichier de cette taille...sorry.";
       //Necessite une modif de php.ini > upload_max_size
       $failed = "1";
@@ -60,9 +54,12 @@ function save($bdd, $target_file, $user)
       header("location: ../index.php?user=$user&error=$failed");
     }
     else{
+      $target_dir = "../docs/";
+      $target_file = $target_dir . basename($_FILES["file"]["name"]);
       move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
-      $link = save($bdd, $target_file);
+      $link = save($bdd, $target_file, $user);
+      $newlink = substr($link, 8);
       rename("$target_file", "$link");
-      header("location: ../Vue/lien.php?lien=$link");
+      header("location: ../Vue/lien.php?lien=$newlink");
       }
   }
